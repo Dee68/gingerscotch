1323d804215e433efe38e052d1ba9a5da0b05fbe
Dimie Egberipou <ddimie283@gmail.com>
2021-12-31 16:05:15 +0200

    initialize project

---
+++ product/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils.safestring import mark_safe
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(null=False,unique=True,max_length=200)
+    #parent = 
+    image = models.ImageField(blank=True, upload_to='categories/')
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def image_tag(self):
+        if self.image:
+            return mark_safe('<img src="%s" height="50" width="50">' %self.image.url)
+        return "No image found"
+
+
+class Product(models.Model):
+    pass
+
+    
+    
