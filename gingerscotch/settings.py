f7809a2dea768c64f676c6bc4157464f5476c10f
Dimie Egberipou <ddimie283@gmail.com>
2022-01-21 19:35:51 +0200

    whitenoise added

--- gingerscotch/settings.py
+++ gingerscotch/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -156,6 +157,7 @@ CKEDITOR_UPLOAD_PATH="uploads/"
 
 #django_heroku.settings(locals())
 django_heroku.settings(config=locals(), staticfiles=False,logging=False)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 
 # Default primary key field type
