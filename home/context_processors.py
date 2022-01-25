aceddb8b306fb5a901edf147ac2d221c79f1b9a4
Dimie Egberipou <ddimie283@gmail.com>
2022-01-25 22:04:11 +0200

    setting applied

--- home/context_processors.py
+++ home/context_processors.py
@@ -2,5 +2,5 @@ from .models import Setting
 
 def access_settings(request):
     """ returns a dictionary """
-    # setting = Setting.objects.get(pk=1)
-    return {}
+    setting = Setting.objects.get(pk=1)
+    return {'setting':setting}
