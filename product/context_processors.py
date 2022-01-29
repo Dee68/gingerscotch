8995ae0e2f4f7e645d229178ee752d8cb2fdab50
Dimie Egberipou <ddimie283@gmail.com>
2022-01-29 21:41:22 +0200

    slider images added

--- product/context_processors.py
+++ product/context_processors.py
@@ -1,7 +1,8 @@
-from .models import Category,Product
+from .models import Category,Product, Picture
 
 def access_parent_category(request):
     """ returns a dictionary """
     pcategories = Category.objects.filter(parent=None)
     products = Product.objects.all()
-    return {'pcategories':pcategories,'products':products}
+    pictures = Picture.objects.filter(title__contains='slider')
+    return {'pcategories':pcategories,'products':products,'pictures':pictures}
